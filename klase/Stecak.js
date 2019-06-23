688f4f245d216793e95d2b3b331bc5d70c2fabe0
Damjan Pavlica <mudroljub@gmail.com>
2019-06-24 00:28:57 +0200

    popravio gresku

--- klase/Stecak.js
+++ klase/Stecak.js
@@ -2,14 +2,14 @@ import {mapa} from './mapa.js'
 const {Marker, LatLng} = google.maps
 
 export default class Stecak extends Marker{
-  constructor(infoWindow, spomen) {
+  constructor(prozor, spomen) {
     super({
       map: mapa,
-      infoWindow,
+      infoWindow: prozor,
       position: new LatLng(spomen.lokacija.lat, spomen.lokacija.lon),
       title: spomen.opis || spomen.naslov,
       icon: 'slike/obelisk.png'
     })
-    this.addListener('click', () => otvoriProzor(infoWindow, marker, spomen.slika))
+    this.addListener('click', () => prozor.otvori(this, spomen.slika))
   }
 }
